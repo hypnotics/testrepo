e2513fccc561ce769954056cc028e4db4498691e
Elias Westerberg <elias.westerberg@gmail.com>
2015-09-09 12:38:00 +0200

    Some wp setup

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
 testrepo
 ========
 
-A test repo
+SW
+========
+
+- Hitta en heltäckande WP-meny
+- Webbfonter
+- Mini-blurb to frontpage (description)
+
+Move CSS / Bootstrap and load them properly from wp
+currently two files loaded from wp-root/css/demo.css
+	     wp-root/bootstrap/css/bootstrap.min.css
+
+
+
+—
+sw/
+
+Bilder i olika format
+3 poster i bredd som kan bli 4 på riktigt breda skärmar
+Rubriken på startsidan STUDIO WITH - input
+Hover visar logo
+
+Logo i botten som laddar fler poster - centrerad
+
+Sticky posts? + senaste (totalt 9 st)
+Infinite scroll
+
+Typsnitt i Rubriken har olika storlekar...
+en är 1.04 av den andra.
+ex (STUDIO WITH) (P5)
+
+-----
+- Dela ikon
+- Pintrest ikon
+-----
+
+Startsida under projekt?
+
+Random ordning på poster
+
+
+
+Meny i vänsterkolumnen
+
+STUDIO WITH
+Branding & Identity
+Signage & Wayfinding
+Editorial
+A Story
+
+Footer i mittkolumnen
+ADDRESS
+Ridhusgatan 6
+903 23 Umeå
+Sweden
+
+TELEPHONE
++46(0)703282392
+
+E-MAIL
+hello@studiowith.se
+
+
+(c)2015 STUDIO WITH 
+- I footer centrerat
---
+++ sw/header.php
@@ -0,0 +1,135 @@
+te for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+	<script>(function(){document.documentElement.className='js'})();</script>
+    
+    
+    <!------------------------------- BEGIN STUDIOWITH ----------------------------------->
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0"> <!-- needed for mobile devices -->
+  
+  	<!-- Import font -->
+  	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+
+  	<!-- Style for Bootstrap -->
+  	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  	
+  	<!-- Custom style for the demo-->
+  	<link rel="stylesheet" href="css/demo.css">
+
+  	<!-- Media Boxes CSS files -->
+	<link rel="stylesheet" href="../plugin/css/magnific-popup.css">
+	<link rel="stylesheet" type="text/css" href="../plugin/css/mediaBoxes.css">
+
+	<style>
+
+		/* == LETS MODIFY SOME COLORS AND STYLE FOR THE DEMO == */
+
+		/* *********** STYLE OF GRID 1 *********** */
+		
+        body{
+            margin-top: 20px !important;
+        }
+        
+        #grid .media-box-date{
+			color: #28ac86;
+		}
+		#grid .media-box-title{
+			font-size: 19px;
+			margin-bottom: 20px;
+		}
+        
+		.thumbnail-overlay>div.aligment>div.aligment {
+    		text-align: left; /* FOR HORIZONTAL ALIGN */
+  		}
+  		.media-boxes-drop-down{
+      		float: right;
+      		margin-left: 8px;
+      	}
+         
+		#grid .media-box-container{
+			-webkit-border-radius: 0px;
+			   -moz-border-radius: 0px;
+		            border-radius: 0px;
+		}
+		#grid .thumbnail-overlay{
+			background: white;
+			padding: 0 20px;
+		}
+		.blue .media-box-title, .blue 
+		.media-box-text,
+		.light-blue .media-box-title, 
+		.light-blue .media-box-text,
+		.red .media-box-title, 
+		.red .media-box-text,
+        .black .media-box-title, 
+		.black .media-box-text,
+		.green .media-box-title, 
+		.green .media-box-text{
+			color: white !important;
+		}
+		.black{
+            background: #323232 !important;
+        }
+        .blue{
+			background: #445E9F !important;
+		}
+		.light-blue{
+			background: #55B1E1 !important;
+		}
+		.red{
+			background: #c03636 !important;
+		}
+		.green{
+			background: #399199 !important;
+		}
+
+
+      	/* *********** HIDE THE "NO MORE ENTRIES" BUTTON *********** */
+		.media-boxes-no-more-entries{
+			display: none;
+		}
+	
+		/* *********** PUSH THE SEARCH TO THE RIGHT *********** */
+		.media-boxes-search{
+      		float: right;
+      	}
+
+      	/* *********** REMOVE SHADOW *********** */
+      	.media-box-container{
+		  -webkit-box-shadow: none !important;
+		     -moz-box-shadow: none !important;
+		       -o-box-shadow: none !important;
+		      -ms-box-shadow: none !important;
+		          box-shadow: none !important;
+		}
+
+	</style>
+    
+    <!------------------------------- END STUDIOWITH ----------------------------------->
+    
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyfifteen' ); ?></a>
+	<div id="content" class="site-content">
+
